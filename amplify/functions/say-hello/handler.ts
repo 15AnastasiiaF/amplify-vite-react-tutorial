25de0884309d79f756a2d502d37478b98a1e6546
Dmitry Bakaev <28220619+bakaevd@users.noreply.github.com>
2025-04-20 00:15:20 -0400

    Adding OpenAI call

--- amplify/functions/say-hello/handler.ts
+++ amplify/functions/say-hello/handler.ts
@@ -1,9 +1,28 @@
 import type { Handler } from 'aws-lambda';
 import type { Schema } from "../../data/resource"
+import { env } from '$amplify/env/say-hello'; 
+import OpenAI from "openai";
+
+const openai = new OpenAI({
+  apiKey: env.OPENAI_API_KEY, 
+});
 
 export const handler: Schema["sayHello"]["functionHandler"] = async (event) => {
     // arguments typed from `.arguments()`
     const { name } = event.arguments
+
+    console.log(`Environment variable NAME: ${env.NAME}`);
+    console.log("Environment variable NAME:", env.OPENAI_API_KEY);
+
+    const prompt = `Generate a greeting for ${name}`;
+    const response = await openai.chat.completions.create({
+      model: "o3-mini",
+      messages: [{ role: "user", content: prompt }],
+    });
+    console.log("OpenAI response:", response.choices[0].message.content?.trim());  
+
+    return response.choices[0].message.content?.trim() || '';
+
     // return typed from `.returns()`
     return `Hello, ${name}!`
   }
--- amplify/functions/say-hello/resource.ts
+++ amplify/functions/say-hello/resource.ts
@@ -1,8 +1,14 @@
-import { defineFunction } from '@aws-amplify/backend';
+import { defineFunction, secret } from '@aws-amplify/backend';
 
 export const sayHello = defineFunction({
   // optionally specify a name for the Function (defaults to directory name)
   name: 'say-hello',
   // optionally specify a path to your handler (defaults to "./handler.ts")
-  entry: './handler.ts'
+  entry: './handler.ts',
+  timeoutSeconds: 30,  // Timeout in seconds (defaults to 3)
+  environment: {
+    NAME: "TestName",
+    //API_ENDPOINT: process.env.API_ENDPOINT
+    OPENAI_API_KEY: secret('openai_api_key')
+  }
 });
